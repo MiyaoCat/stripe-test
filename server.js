707c23dc80906d66d28527b9af6465f6a95e1d12
John Miyao <jjmiyao@gmail.com>
2024-06-10 13:08:26 -0700

    added homepage

--- server.js
+++ server.js
@@ -2,16 +2,22 @@
 const stripe = require('stripe')('sk_test_51POXDuCYeKApvPddSAHznZfyVwYCnKHVxvivsWo1daQ80DBM1npelsQnGdX9YPqdlQDBCVqjopaZ7KXkdiTIIrGH00onkqzubE');
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-const YOUR_DOMAIN = 'http://localhost:4242';
+const YOUR_DOMAIN = 'http://localhost:2024';
+
+app.get('/', (req, res) => {
+    res.render('home'); // Render the 'home.ejs' template
+});
 
 app.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'price_1POwS4CYeKApvPddTuD12dF7',
+        price: 'price_1POqO2CYeKApvPddvD0XBDdX',
         quantity: 1,
       },
     ],
@@ -25,4 +31,4 @@ app.post('/create-checkout-session', async (req, res) => {
 });
 
 
-app.listen(4242, () => console.log('Running on port 4242'));
+app.listen(2024, () => console.log('Running on port 2024'));
