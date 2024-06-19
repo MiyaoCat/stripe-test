d25a601a974f302befa18a9047f783322af97b8f
John Miyao <jjmiyao@gmail.com>
2024-06-19 11:06:03 -0700

    Moved test key to .env file

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 // This is your test secret API key.
-const stripe = require('stripe')('sk_test_51POXDuCYeKApvPddSAHznZfyVwYCnKHVxvivsWo1daQ80DBM1npelsQnGdX9YPqdlQDBCVqjopaZ7KXkdiTIIrGH00onkqzubE');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+// console.log(`Stripe Secret Key: ${stripeSecretKey}`); 
+const stripe = require('stripe')(stripeSecretKey);
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -10,7 +16,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('public', path.join(__dirname, 'public'));
 app.use(express.static('public'));
 
-const YOUR_DOMAIN = 'https://stripe-test-hazel.vercel.app/';
+//live site https://stripe-test-hazel.vercel.app/
+const YOUR_DOMAIN = 'http://localhost:4242';
 
 app.get('/', (req, res) => {
     res.render('home'); 
@@ -21,21 +28,32 @@ app.get('/checkout', (req, res) => {
 });
 
 app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'price_1POqO2CYeKApvPddvD0XBDdX',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/success`,
-    cancel_url: `${YOUR_DOMAIN}/cancel`,
-    automatic_tax: {enabled: true},
-  });
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: 'price_1POqO2CYeKApvPddvD0XBDdX',
+          quantity: 1,
+        },
+        {
+          price: 'price_1POwS4CYeKApvPddTuD12dF7',
+          quantity: 1,
+        }
+      ],
+      mode: 'payment',
+      success_url: `${YOUR_DOMAIN}/success`,
+      cancel_url: `${YOUR_DOMAIN}/cancel`,
+      automatic_tax: {enabled: true},
+    });
 
   res.redirect(303, session.url);
+  }catch (error) {
+    console.error(`Error creating checkout session: ${error.message}`);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.get('/cancel', (req, res) => {
@@ -47,4 +65,4 @@ app.get('/success', (req, res) => {
 });
 
 
-app.listen(2024, () => console.log('Running on port 2024'));
+app.listen(4242, () => console.log('Running on port 4242'));
