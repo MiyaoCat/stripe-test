8b1e8e266e0950ff93f5377a288264e7e3ea3204
John Miyao <jjmiyao@gmail.com>
2024-07-03 09:23:27 -0700

    Removed cashapp

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/checkout', (req, res) => {
 app.post('/create-checkout-session', async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card', 'cashapp'],
+      payment_method_types: ['card'],
       mode: 'payment',
       line_items: [
         {
